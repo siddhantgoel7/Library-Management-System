144a28d89ed00d98cd74c653b1d2e94f2f8b3d6a
Siddhant Goel <89402613+siddhantgoel7@users.noreply.github.com>
2022-10-01 20:19:43 +0530

    Create logout.py

---
+++ logout.py
@@ -0,0 +1,6 @@
+def HandleLogout(request):
+        del request.session['is_logged']
+        del request.session["user_id"] 
+        logout(request)
+        messages.success(request, " Successfully logged out")
+        return redirect('dashboard')
